7c0bba15081caf6b341a3bdb12131690fc36a3bd
p-moon <pf@jdk.plus>
2024-02-21 11:11:17 +0800

    修复in针对字符串类型的问题

--- src/main/java/plus/jdk/milvus/common/operator/IOperatorComputer.java
+++ src/main/java/plus/jdk/milvus/common/operator/IOperatorComputer.java
@@ -7,7 +7,7 @@ public interface IOperatorComputer {
     String compute(String leftValue, Object rightValue, Class<?> clazz) throws MilvusException;
 
     default Object formatRvalue(Object rvalue) {
-        if(rvalue instanceof String) {
+        if(!(rvalue instanceof Integer || rvalue instanceof Long || rvalue instanceof Float || rvalue instanceof Double)) {
             return String.format("\"%s\"", rvalue);
         }
         return rvalue;
